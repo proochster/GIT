c15012d8d15603b69f29e3fbfb474e296354adb7
Paul Kosinski <pawelkosi@gmail.com>
2015-05-20 20:05:52 +0100

    Added two style files for login page. Cleaned up config.php, header.php and login_content.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://backup-pc.dyndns-server.com/bikdemo/';
+$config['base_url']	= 'http://backup-pc.dyndns-server.com/stage/bik/';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/header.php
+++ application/views/header.php
@@ -14,8 +14,7 @@
         
         <!-- Stylesheets -->
         <link rel="stylesheet" href="//code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css">
-		<link rel="stylesheet" href="http://backup-pc.dyndns-server.com/bikdemo/styles/main.css" type="text/css" media="screen">
-		<!-- <link rel="stylesheet" href="/profoundgrid-master/css/examples/fluid.css" type="text/css" media="screen" /> -->
+		<link rel="stylesheet" href="http://backup-pc.dyndns-server.com/stage/bik/styles/main.css" type="text/css" media="screen">
 		
 		<!-- Fonts -->
 		<link href='http://fonts.googleapis.com/css?family=Raleway:500,900,200' rel='stylesheet' type='text/css'>
@@ -24,15 +23,14 @@
    		<link href='http://fonts.googleapis.com/css?family=Exo:600' rel='stylesheet' type='text/css'>
 
 		<!-- Javascript -->
-		<script src="/bikdemo/js/vendor/modernizr-2.8.3.min.js"></script> 
+		<script src="/stage/bik/js/vendor/modernizr-2.8.3.min.js"></script> 
 		<script src="//code.jquery.com/jquery-1.10.2.js"></script>
   		<script src="//code.jquery.com/ui/1.11.3/jquery-ui.js"></script>
   		<script type="text/javascript" src="https://www.google.com/jsapi"></script>
   		<script src="//ajax.googleapis.com/ajax/libs/jquery/{{JQUERY_VERSION}}/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="../js/vendor/jquery-{{JQUERY_VERSION}}.min.js"><\/script>')</script>
-        <script src="/bikdemo/js/plugins.js"></script>
-        <script src="/bikdemo/js/main.js"></script>
-        <!-- <script type="text/javascript" src="/markform/js/jquery-1.8.3.min.js"></script> -->
+        <script src="/stage/bik/js/plugins.js"></script>
+        <script src="/stage/bik/js/main.js"></script>
 		
 		 <script>
 				$(function() {
@@ -44,4 +42,4 @@
 		</script>	
 </head>
 <body>
-<img src="http://backup-pc.dyndns-server.com/bikdemo/img/logo-side.png" id="logo"  />
+<img src="http://backup-pc.dyndns-server.com/stage/bik/img/logo-side.png" id="logo"  />
