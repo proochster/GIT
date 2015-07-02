3e6e0f21a0354052b792adcc4f930ac767a130ff
proochster <proochster@gmail.com>
2015-07-02 23:10:31 +0100

    initial media queries

--- application/views/head.php
+++ application/views/head.php
@@ -2,11 +2,10 @@
 <html class="no-js" lang="en">
 <head>
 		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Benefit In Kind - FPS</title>
         <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
         <!-- Place favicon.ico in the root directory -->
