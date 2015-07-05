2c8c01910144a69b23f9ceb8e641780c4cc1360d
proochster <proochster@gmail.com>
2015-07-05 23:19:38 +0100

    Table styles - table 2 & 4

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,10 @@
-	<div id="footer_fps">
-		<h4>FPS 2015</h4>
-	</div>
+    <div class="grid" id="footer_fps">
+        <div class="grid-12">
+            <h4>FPS 2015</h4>
+        </div>
+        <!--div class="grid-12">
+            <p><i class="fa fa-arrow-circle-up"></i> Back to top</p>
+        </div-->
+    </div>
 </head>
 <body>
--- application/views/mileage_content.php
+++ application/views/mileage_content.php
@@ -1,96 +1,108 @@
-       	
 <div id="container">
-	<h1>MILEAGE DATA INPUT</h1>
-	<?= form_open('forms/mileage'); ?>
-	
-	<table id="table4" width="100%">
-		<thead>
-			<tr>
-				<td class="date"><h4>Date</h4></td>
-				<td class="reg"><h4>Registration</h4></td>
-				<td class="from"><h4>From</h4></td>
-				<td class="to"><h4>To</h4></td>
-				<td class="client"><h4>Client</h4></td>
-				<td class="return"><h4>Return</h4></td>
-				<td class="mileage"><h4>Milage</h4></td>
-			</tr>
-		</thead>
-			<tr>			 
-			<td>
-				<input type="text" id="datepicker" name="date" />
-			</td>
-			<td>
-				<select name="reg">
-						<?php	
-							foreach($registration as $row){
-								echo "<option value='".$row->registration."'>".$row->registration."</option>";
-							} 						 
-						?>
-				</select>
-			</td>
-			<td>				
-				<input placeholder="From" type="text" name="from" />
-			</td>
-			<td>				
-				<input placeholder="To" type="text" name="to" />
-			</td>
-			<td>				
-				<input placeholder="Client" type="text" name="client" />
-			</td>			
-			<td>
-				<select name="return">
-					<option value = "YES">YES</option>
-					<option value = "NO">NO</option>
-				</select>
-			</td>			
-			<td>
-				<input placeholder="Mileage" type="number" name="mileage" />
-			</td>			
-		</tr>
-		</table>
-		
-		<div class="errormsg">
-			<p><?php echo form_error('date'); ?></p>
-			<p><?php echo form_error('from'); ?></p>
-			<p><?php echo form_error('to'); ?></p>
-			<p><?php echo form_error('client'); ?></p>
-			<p><?php echo form_error('mileage'); ?></p>
-		</div>
-		
-	<div class="center"><input type="submit" id="submit1" class="submit-button"/></div>
-	
-	<?= form_close(); ?>
-	
-	<table id="table2" width="100%">
-		<thead>
-			<tr>
-				<td class="date"><h4>Date</h4></td>
-				<td class="reg"><h4>From</h4></td>
-				<td class="model"><h4>To</h4></td>
-				<td class="fuel"><h4>Client</h4></td>
-				<td class="from"><h4>Return</h4></td>
-				<td class="to"><h4>Registration</h4></td>
-				<td class="client"><h4>Model</h4></td>
-				<td class="return"><h4>Fuel</h4></td>
-				<td class="mileage"><h4>Milage</h4></td>
-			</tr>
-		</thead>
-			
-	<?php
-	
-	foreach($results as $row){
-		echo "<tr><td>".$row->date."</td>";
-		echo "<td>".$row->from."</td>";
-		echo "<td>".$row->to."</td>";
-		echo "<td>".$row->client."</td>";
-		echo "<td>".$row->return."</td>";		
-		echo "<td>".$row->reg."</td>";
-		echo "<td>".$row->car."</td>";
-		echo "<td>".$row->fuel."</td>";
-		echo "<td>".$row->mileage."</td>";
-	} 
-	 
-	?>
-	</table>
+    <div class="clearfix">    
+        <h1>MILEAGE DATA INPUT</h1>
+        <?= form_open('forms/mileage'); ?>
 
+        <div class="grid">
+            <div class="grid-24 scroll">
+                <table id="table4">
+                    <thead>
+                        <tr>
+                            <td class="date"><h4>Date</h4></td>
+                            <td class="reg"><h4>Registration</h4></td>
+                            <td class="from"><h4>From</h4></td>
+                            <td class="to"><h4>To</h4></td>
+                            <td class="client"><h4>Client</h4></td>
+                            <td class="return"><h4>Return</h4></td>
+                            <td class="mileage"><h4>Milage</h4></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>			 
+                            <td>
+                                <input type="text" id="datepicker" name="date" />
+                            </td>
+                            <td>
+                                <select name="reg">
+                                        <?php	
+                                            foreach($registration as $row){
+                                                echo "<option value='".$row->registration."'>".$row->registration."</option>";
+                                            } 						 
+                                        ?>
+                                </select>
+                            </td>
+                            <td>				
+                                <input placeholder="From" type="text" name="from" />
+                            </td>
+                            <td>				
+                                <input placeholder="To" type="text" name="to" />
+                            </td>
+                            <td>				
+                                <input placeholder="Client" type="text" name="client" />
+                            </td>			
+                            <td>
+                                <select name="return">
+                                    <option value = "YES">YES</option>
+                                    <option value = "NO">NO</option>
+                                </select>
+                            </td>			
+                            <td>
+                                <input placeholder="Mileage" type="number" name="mileage" />
+                            </td>			
+                        </tr>
+                    </tbody>
+                </table>
+
+                <div class="errormsg">
+                    <p><?php echo form_error('date'); ?></p>
+                    <p><?php echo form_error('from'); ?></p>
+                    <p><?php echo form_error('to'); ?></p>
+                    <p><?php echo form_error('client'); ?></p>
+                    <p><?php echo form_error('mileage'); ?></p>
+                </div>            
+            </div>
+
+            <div class="grid-24 center">
+                <input type="submit" id="submit1" class="large link_button"/>                 
+            </div>
+        </div>    
+
+        <?= form_close(); ?>
+
+        <div class="grid">
+            <div class="grid-24 scroll">
+                <table id="table2">
+                    <thead>
+                        <tr>
+                            <td class="date"><h4>Date</h4></td>
+                            <td class="reg"><h4>From</h4></td>
+                            <td class="model"><h4>To</h4></td>
+                            <td class="fuel"><h4>Client</h4></td>
+                            <td class="from"><h4>Return</h4></td>
+                            <td class="to"><h4>Registration</h4></td>
+                            <td class="client"><h4>Model</h4></td>
+                            <td class="return"><h4>Fuel</h4></td>
+                            <td class="mileage"><h4>Milage</h4></td>
+                        </tr>
+                    </thead>
+
+                    <?php
+
+                    foreach($results as $row){
+                        echo "<tr><td><p>".$row->date."</p></td>";
+                        echo "<td><p>".$row->from."</p></td>";
+                        echo "<td><p>".$row->to."</p></td>";
+                        echo "<td><p>".$row->client."</p></td>";
+                        echo "<td><p>".$row->return."</p></td>";		
+                        echo "<td><p>".$row->reg."</p></td>";
+                        echo "<td><p>".$row->car."</p></td>";
+                        echo "<td><p>".$row->fuel."</p></td>";
+                        echo "<td><p>".$row->mileage."</p></td></tr>";
+                    } 
+
+                    ?>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
